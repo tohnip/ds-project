a8a440275abf795c88bb39317d3c467e0a4328a5
Patrik <Patrik>
2024-02-22 16:40:29 +0200

    Removed old code related to http based traffic demonstration. Also cleaned other small things.

--- src/consumer/get_material.py
+++ src/consumer/get_material.py
@@ -1,16 +1,15 @@
-import requests
-import logging
-import time
-import sys
 import json
+import logging
+import requests
 import socketio
+import sys
+import time
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 root.addHandler(handler)
-
 
 hostname = "localhost"
 hostname = "server"  # comment this out if you want to run without docker
@@ -34,42 +33,9 @@ def main():
         while True:
             response = sio.receive()
             logging.info(response[1])
-            #logging.info({"event_name": response[0], "data": response[1]})
-
-
-
-
-    return
-
-    got_healthy_response = True
-    while got_healthy_response:
-        got_healthy_response = False
-
-        response = requests.get("http://server:15000/available_content")
-        if response.status_code != 200:
-            logging.warning({"message": "get request failed", "response": response})
-
-        json_content = json.loads(response.content)
-        logging.info({"message": "received content", "content": json_content})
-
-        for story_name in json_content["Available stories"]:
-            part_ind = 1
-            while True:
-                response = requests.get(f"http://server:15000/story/{story_name}/{part_ind}")
-                if response.status_code != 200:
-                    logging.warning({"message": "get request failed", "response content": response.content})
-                    break
-                
-                got_healthy_response = True
-
-                json_response = json.loads(response.content)
-
-                logging.info(json_response["new part"])
-                part_ind = int(json_response["next_id"])
-
-                time.sleep(2)
 
 
 if __name__ == '__main__':
+    # delay the start so that server and broadcaster have time to start before this
     time.sleep(3)
     main()
